3dd1660ffffe3dbac8a7cfd9da6458e7b855895e
CNJerry-IvanovVyacheslav <syava11212@yandex.ru>
2023-12-04 14:27:32 +0300

    Отшлифовка завершения и первые попытки все это грамотно структурировать

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
+
+    static int allFriends;
+    static double allCost;
     public static void main(String[] args) {
+
         checkYourFriends();
+        result();
     }
 
     // Проверяет на сколько человек разделить счет
@@ -16,37 +21,48 @@ public class Main {
                 System.out.println("Нет смысла делить счет когда вы одни! Попробуйте другое число.");
             } else if (howManyFriends <= 0) {
                 System.out.println("Нарушены законы физики! Прекратите! Введите реальное число людей.");
-            } else { calculate(); }
-        }
-    }
+            } else {calculate(); allFriends = howManyFriends; } // добавить цикл?
 
-    // Создает список товаров и их общую стоимость
-    public static void calculate() {
-        double cost = 0.00;
-        String productLine = "";
-        while (true) {
-            System.out.println("Какой товар вы купили?");
-
-            String product = scanner.next();
-            productLine = productLine + " " + product;
-            System.out.println("Товар " + product + " успешно добавлен в список!");
-
-            System.out.println("Сколько это стоило?");
-            cost = cost + scanner.nextDouble();
-
-            System.out.println("Желаете добавить новый товар?");
-            String isEnough = scanner.next();
-            if (isEnough.equalsIgnoreCase("Завершить")) {
-                System.out.println("Ваш список покупок состоит из: " + productLine + ". Общей стоимостью: " + cost + " рублей.");
-                break;
-            }
         }
 
-
-
-
-
     }
+
+  public static void calculate() {  // Создает список товаров и их общую стоимость
+      double cost = 0.00;
+      String productLine = "";
+
+          while (true) {
+              System.out.println("Какой товар вы купили?");
+
+              String product = scanner.next();
+              productLine = productLine + " \n" + product;
+              System.out.println("Товар " + product + " успешно добавлен в список!");
+
+              System.out.println("Сколько это стоило?");
+              cost = cost + scanner.nextDouble();
+
+              System.out.println("Желаете добавить новый товар?");
+              String isEnough = scanner.next();
+              if (isEnough.equalsIgnoreCase("Завершить")) {
+                  System.out.println("Ваш список покупок состоит из: " + productLine + ".\nОбщей стоимостью: " + cost + " рублей.");
+                  allCost = cost;
+                  break;
+              }
+          }
+
+  }
+        public static void result() {
+            double equallySum = allCost / allFriends;
+            double equallySumInt = Math.floor(equallySum);
+            String rub = " рубль.";
+            String rubs = " рублей.";
+
+
+        /*   if (equallySum %= 1) {System.out.println("Сумма, которую должен заплатить каждый = " + equallySum + " рубль.");
+             } else { System.out.println("Сумма, которую должен заплатить каждый = " + equallySum + " рублей.");} */
+
+        }
+
 
 
 
